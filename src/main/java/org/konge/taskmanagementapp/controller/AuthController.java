f281ea2bd6dfd39f7843f17c1900745bf0e2b5cc
ivanmacieldxz <ivanemanuelmaciel@gmail.com>
2026-02-09 19:56:26 -0300

    feat: auth controller with user register mapping under /api/auth/register

---
+++ src/main/java/org/konge/taskmanagementapp/controller/AuthController.java
@@ -0,0 +1,29 @@
+package org.konge.taskmanagementapp.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.konge.taskmanagementapp.api.dto.UserRegistrationDTO;
+import org.konge.taskmanagementapp.api.model.User;
+import org.konge.taskmanagementapp.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@Valid @RequestBody UserRegistrationDTO dto) {
+        User registeredNewUser = userService.registerUser(dto);
+
+        return new ResponseEntity<>(registeredNewUser, HttpStatus.CREATED);
+    }
+
+}
