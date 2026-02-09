d9f8d67ed8e48d5374e967859379dccf6bc8ccc1
ivanmacieldxz <ivanemanuelmaciel@gmail.com>
2026-02-09 19:45:14 -0300

    feat: user service with register functionality

---
+++ src/main/java/org/konge/taskmanagementapp/service/UserService.java
@@ -0,0 +1,40 @@
+package org.konge.taskmanagementapp.service;
+
+import lombok.RequiredArgsConstructor;
+import org.konge.taskmanagementapp.api.dto.UserRegistrationDTO;
+import org.konge.taskmanagementapp.api.model.User;
+import org.konge.taskmanagementapp.api.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+    
+    public User registerUser(UserRegistrationDTO dto) {
+        String registrationUsername = dto.username();
+        String registrationEmail = dto.email();
+
+        if (userRepository.existsByEmail(registrationEmail))
+            throw new RuntimeException("Mail is already registered for other user.");
+
+        if (userRepository.existsByUsername(registrationUsername))
+            throw new RuntimeException("Username is already taken.");
+
+
+        String encodedPassword = passwordEncoder.encode(
+                dto.password()
+        );
+
+        User user = User.builder()
+                .username(registrationUsername)
+                .email(registrationEmail)
+                .password(encodedPassword)
+                .build();
+        
+        return userRepository.save(user);
+    }
+}
