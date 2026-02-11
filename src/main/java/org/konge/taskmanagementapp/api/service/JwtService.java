55353d3964b33bd66f28a6158368b05cceb6e12c
ivanmacieldxz <ivanemanuelmaciel@gmail.com>
2026-02-11 18:21:36 -0300

    feat: added token utility methods to JwtService

--- src/main/java/org/konge/taskmanagementapp/api/service/JwtService.java
+++ src/main/java/org/konge/taskmanagementapp/api/service/JwtService.java
@@ -31,4 +31,29 @@ public class JwtService {
                 .signWith(SECRET_KEY)
                 .compact();
     }
+
+    public String extractUsername(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parser()
+                .verifyWith(SECRET_KEY)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public boolean isTokenValid(String token, String userEmail) {
+        final String username = extractUsername(token);
+
+        return username.equals(userEmail) && !isTokenExpired(token);
+    }
+
+    public boolean isTokenExpired(String token) {
+        return extractAllClaims(token).getExpiration().before(new Date());
+    }
+
+
 }
