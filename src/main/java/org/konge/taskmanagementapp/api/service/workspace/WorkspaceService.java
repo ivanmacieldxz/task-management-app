26248d3670b7a7aaaf2c5232676db32f151bc6a3
ivanmacieldxz <ivanemanuelmaciel@gmail.com>
2026-02-16 15:40:17 -0300

    feat: WorkspaceService definition

---
+++ src/main/java/org/konge/taskmanagementapp/api/service/workspace/WorkspaceService.java
@@ -0,0 +1,46 @@
+package org.konge.taskmanagementapp.api.service.workspace;
+
+import lombok.RequiredArgsConstructor;
+import org.konge.taskmanagementapp.api.model.user.User;
+import org.konge.taskmanagementapp.api.model.workspaces.Workspace;
+import org.konge.taskmanagementapp.api.repository.user.UserRepository;
+import org.konge.taskmanagementapp.api.repository.workspace.WorkspaceRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class WorkspaceService {
+
+    private final WorkspaceRepository workspaceRepository;
+    private final UserRepository userRepository;
+
+    @Transactional(readOnly = true)
+    public List<Workspace> findWorkspacesForCurrentUser() {
+        User user = getCurrentUser();
+        return workspaceRepository.findWorkspaceByOwnerId(user.getId());
+    }
+
+    @Transactional
+    public Workspace createWorkspace(Workspace workspace) {
+        User user = getCurrentUser();
+
+        workspace.setOwner(user);
+
+        return workspaceRepository.save(workspace);
+    }
+
+    private User getCurrentUser() {
+        String userEmail =  SecurityContextHolder.getContext()
+                .getAuthentication()
+                .getName();
+
+        return userRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("No user with email: " + userEmail + " found."));
+    }
+
+}
