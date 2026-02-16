c794253573db8f358356169d7390267b5fe4a69f
ivanmacieldxz <ivanemanuelmaciel@gmail.com>
2026-02-16 16:53:20 -0300

    feat: WorkspaceController with creation and listing endpoints

---
+++ src/main/java/org/konge/taskmanagementapp/api/controller/workspace/WorkspaceController.java
@@ -0,0 +1,54 @@
+package org.konge.taskmanagementapp.api.controller.workspace;
+
+import lombok.RequiredArgsConstructor;
+import org.konge.taskmanagementapp.api.dto.workspace.WorkspaceCreationRequestDTO;
+import org.konge.taskmanagementapp.api.dto.workspace.WorkspaceResponseDTO;
+import org.konge.taskmanagementapp.api.model.workspaces.Workspace;
+import org.konge.taskmanagementapp.api.service.workspace.WorkspaceService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/workspaces")
+@RequiredArgsConstructor
+public class WorkspaceController {
+
+    private final WorkspaceService workspaceService;
+
+    @PostMapping
+    public ResponseEntity<WorkspaceResponseDTO> createWorkspace(@RequestBody WorkspaceCreationRequestDTO workspaceCreationRequest) {
+        Workspace workspace = Workspace.builder()
+                .name(workspaceCreationRequest.name())
+                .description(workspaceCreationRequest.description())
+                .build();
+
+        Workspace createdWorkspace = workspaceService.createWorkspace(workspace);
+
+        return new ResponseEntity<>(
+                mapToResponse(createdWorkspace),
+                HttpStatus.CREATED
+        );
+    }
+
+    @GetMapping
+    public ResponseEntity<List<WorkspaceResponseDTO>> getAllWorkspaces() {
+        List<WorkspaceResponseDTO> responses = workspaceService.findWorkspacesForCurrentUser()
+                .stream()
+                .map(this::mapToResponse)
+                .toList();
+
+        return ResponseEntity.ok(responses);
+    }
+
+    private WorkspaceResponseDTO mapToResponse(Workspace workspace) {
+        return new WorkspaceResponseDTO(
+                workspace.getId(),
+                workspace.getName(),
+                workspace.getDescription(),
+                workspace.getCreatedAt()
+        );
+    }
+}
