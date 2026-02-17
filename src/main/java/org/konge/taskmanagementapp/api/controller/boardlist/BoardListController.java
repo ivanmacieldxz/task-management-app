b55a4c7102a04566c01299b0321571adc4d7248f
ivanmacieldxz <ivanemanuelmaciel@gmail.com>
2026-02-16 21:13:40 -0300

    feat: BoardListController with create and move endpoints

---
+++ src/main/java/org/konge/taskmanagementapp/api/controller/boardlist/BoardListController.java
@@ -0,0 +1,55 @@
+package org.konge.taskmanagementapp.api.controller.boardlist;
+
+import lombok.RequiredArgsConstructor;
+import org.konge.taskmanagementapp.api.dto.boardlist.BoardListCreationRequestDTO;
+import org.konge.taskmanagementapp.api.dto.boardlist.BoardListMoveRequestDTO;
+import org.konge.taskmanagementapp.api.dto.boardlist.BoardListResponseDTO;
+import org.konge.taskmanagementapp.api.model.boardlist.BoardList;
+import org.konge.taskmanagementapp.api.service.boardlist.BoardListService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/workspaces/{workspaceId}/lists")
+@RequiredArgsConstructor
+public class BoardListController {
+
+    private final BoardListService boardListService;
+
+    @PostMapping
+    public ResponseEntity<BoardListResponseDTO> createList(
+            @PathVariable Long workspaceId,
+            @RequestBody BoardListCreationRequestDTO request
+    ) {
+        BoardList boardList = boardListService.createList(workspaceId, request.name());
+
+        return new ResponseEntity<>(
+                mapToResponse(boardList),
+                HttpStatus.CREATED
+        );
+    }
+
+    @PatchMapping("/{listId}/move")
+    public ResponseEntity<BoardListResponseDTO> moveList(
+            @PathVariable Long listId,
+            @RequestBody(required = false) BoardListMoveRequestDTO boardListMoveRequestDTO
+    ) {
+        BoardList movedList = boardListService.moveListBetween(
+                listId,
+                boardListMoveRequestDTO.prevListPosition(),
+                boardListMoveRequestDTO.nextListPosition()
+        );
+
+        return ResponseEntity.ok(mapToResponse(movedList));
+    }
+
+    private BoardListResponseDTO mapToResponse(BoardList boardList) {
+        return BoardListResponseDTO.builder()
+                .id(boardList.getId())
+                .name(boardList.getName())
+                .positionInBoard(boardList.getPositionInBoard())
+                .workspace(boardList.getWorkspace().getId())
+                .build();
+    }
+}
