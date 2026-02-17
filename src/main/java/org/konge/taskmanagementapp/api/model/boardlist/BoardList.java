e03a36bc01cbade3402e0d07a19c5a93d488f68d
ivanmacieldxz <ivanemanuelmaciel@gmail.com>
2026-02-17 14:38:37 -0300

    bugfix: added annotation that was preventing inserts for BoardList entities

--- src/main/java/org/konge/taskmanagementapp/api/model/boardlist/BoardList.java
+++ src/main/java/org/konge/taskmanagementapp/api/model/boardlist/BoardList.java
@@ -6,12 +6,14 @@ import org.konge.taskmanagementapp.api.model.task.Task;
 import org.konge.taskmanagementapp.api.model.workspace.Workspace;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
 @Table(name = "board_list")
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
 @Builder
